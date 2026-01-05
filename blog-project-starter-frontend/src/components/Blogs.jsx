0bd065561d7c439a596f5c759efafad90e2f4049
PradeepSingh24 <pradeep.bps24@gmail.com>
2026-01-05 14:16:13 +0530

    after backend url

--- blog-project-starter-frontend/src/components/Blogs.jsx
+++ blog-project-starter-frontend/src/components/Blogs.jsx
@@ -21,7 +21,7 @@ function Blogs() {
             }
         })
         window.scrollTo(0, 0);
-        axios.get("http://localhost:5000/api/blogs").then((res) => {
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}api/blogs`).then((res) => {
             console.log(res.data)
             setBlogs(res.data)
         }).catch(() => {
@@ -37,10 +37,10 @@ function Blogs() {
 
     const handleLike = async (blog_id) => {
         try {
-            const response = await axios.patch(`http://localhost:5000/api/blogs/like/${blog_id}`);
+            const response = await axios.patch(`${process.env.REACT_APP_BACKEND_URL}api/blogs/like/${blog_id}`);
             // After successfully updating the likes count in the backend, fetch the updated list of blogs
             if (response.status === 200) {
-                axios.get("http://localhost:5000/api/blogs").then((res) => {
+                axios.get(`${process.env.REACT_APP_BACKEND_URL}api/blogs`).then((res) => {
                     console.log(res.data)
                     setBlogs(res.data)
                 }).catch(() => {
@@ -59,10 +59,10 @@ function Blogs() {
 
 
         const likes = 0
-        axios.post("http://localhost:5000/api/blogs", { newTitle, date, newContent, likes }).then((res) => {
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}api/blogs`, { newTitle, date, newContent, likes }).then((res) => {
             console.log(res.data)
 
-            axios.get("http://localhost:5000/api/blogs").then((res) => {
+            axios.get(`${process.env.REACT_APP_BACKEND_URL}api/blogs`).then((res) => {
                 console.log(res.data)
                 setBlogs(res.data)
             }).catch(() => {
